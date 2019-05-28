17fed6f4a2da39d073276389ed478c9116e9b4dc
Piotrek Dobrowolski <piotrekdobroolski87@gmail.com>
2019-05-28 21:26:53 +0200

    common

--- src/test/java/common/CommonMethods.java
+++ src/test/java/common/CommonMethods.java
@@ -1,4 +1,73 @@
 package common;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
+import java.time.Duration;
+import java.util.List;
+
 public class CommonMethods {
+
+    private WebDriver driver;
+    private Duration timeout = Duration.ofSeconds(10);
+    private Duration pollingTime = Duration.ofMillis(500);
+
+    public CommonMethods(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void waitForPictureChange(WebElement element, String attribute, String attributeValue){
+        waitFor().until(ExpectedConditions.not(ExpectedConditions.attributeContains(element,attribute,attributeValue)));
+    }
+
+    public void waitForViibility(WebElement element){
+        waitFor().until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void waitForClick(WebElement element){
+        waitFor().until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public void waitForVisibilityOfCollection(List<WebElement> collectionOfElements){
+        for(WebElement element : collectionOfElements){
+            waitFor().until(ExpectedConditions.visibilityOf(element));
+        }
+    }
+
+    public void waitForPresence(By locator,String text){
+        waitFor().until(ExpectedConditions.not(ExpectedConditions.textToBe(locator,text)));
+    }
+
+    public void moveToElement(WebElement element) {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element);
+        actions.perform();
+    }
+
+    public void checkCurrentUrl(String url) {
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, url,"Adres strony nie zgadza się");
+    }
+
+    public void waitForUrl(String url){
+        waitFor().until(ExpectedConditions.urlToBe(url));
+    }
+
+    public void log(String message){
+        System.out.println("[LOG] " + message);
+    }
+
+    private Wait waitFor(){
+        Wait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(timeout)
+                .pollingEvery(pollingTime)
+                .ignoring(NoSuchElementException.class);
+        return wait;
+    }
 }
--- src/test/java/common/SetBrowser.java
+++ src/test/java/common/SetBrowser.java
@@ -1,4 +1,48 @@
 package common;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class SetBrowser {
+
+    private WebDriver driver;
+
+    public WebDriver openBrowserWindow(String browser, int width, int height, String url){
+        Dimension dimension = new Dimension(width,height);
+        setDriver(browser);
+
+        driver.manage().deleteAllCookies();
+
+        driver.manage()
+                .window()
+                .setSize(dimension);
+        driver.get(url);
+
+        return driver;
+    }
+
+    private void setDriver(String browser) {
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver()
+                        .setup();
+                this.driver = new FirefoxDriver();
+                break;
+            case "chrome":
+                WebDriverManager.chromedriver()
+                        .setup();
+                this.driver = new ChromeDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver()
+                        .setup();
+                this.driver = new EdgeDriver();
+                break;
+
+        }
+    }
 }
